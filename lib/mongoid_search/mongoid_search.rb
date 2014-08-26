708cfa428dd60e25988b90c9e03502f7746c9681
Ninge_UA <ningen.ua@gmail.com>
2014-08-26 16:00:38 +0300

    add screening for extract options

--- lib/mongoid_search/mongoid_search.rb
+++ lib/mongoid_search/mongoid_search.rb
@@ -71,7 +71,9 @@ module Mongoid::Search
         {
           :match              => options[:match]              || Mongoid::Search.match,
           :allow_empty_search => options[:allow_empty_search] || Mongoid::Search.allow_empty_search,
-          :relevant_search    => options[:relevant_search]    || Mongoid::Search.relevant_search
+          :relevant_search    => options[:relevant_search]    || Mongoid::Search.
+          :screening          => options[:screening]        
+
         }
       end
 
